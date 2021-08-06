9993ba808b78d526c3903ec7576d350135fcf957
zehndert <56029666+zehndert@users.noreply.github.com>
2021-08-06 10:06:38 +0200

    Update onLoad.R

--- R/onLoad.R
+++ R/onLoad.R
@@ -4,7 +4,7 @@
 #' @noRd
 #'
 .onLoad <- function(...) {
-  shiny::addResourcePath("shinytreeview", system.file("assets", package = "shinytreeview"))
+  shiny::addResourcePath("shinytreeviewCustom", system.file("assets", package = "shinytreeview"))
   shiny::registerInputHandler("treeview.name", function(data, ...) {
     if (is.null(data) || length(data) < 1) {
       NULL
